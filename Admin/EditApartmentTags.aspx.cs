05f12ebca30a0435bda2e9d688dd43878b991ff5
Ivan <someonenetko@hotmail.com>
2023-01-12 02:28:18 +0100

    Added basic codebehind to editapartmenttags.

--- Admin/EditApartmentTags.aspx.cs
+++ Admin/EditApartmentTags.aspx.cs
@@ -1,4 +1,5 @@
-﻿using DAL.Models;
+﻿using DAL.DatabaseAccess;
+using DAL.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,10 +11,46 @@ namespace Admin
 {
 	public partial class EditApartmentTags : System.Web.UI.Page
 	{
-		protected void Page_Load(object sender, EventArgs e)
-		{
+        private static IList<Tag> _allTags;
+        private static ISet<Tag> _selectedtags;
 
-		}
+        protected void Page_Load(object sender, EventArgs e)
+		{
+            if ((string)Session["user"] != "admin")
+            {
+                Response.Redirect("Default.aspx");
+            }
+            if (!IsPostBack)
+            {
+                _allTags = DbAccess.LoadTags();
+
+                if (Session["apartment_id"] != null)
+                {
+                    int selected_id = (int)Session["apartment_id"];
+                    _selectedtags = new HashSet<Tag>(DbAccess.LoadTagsForApartment(selected_id));
+                    _selectedtags.ToList().ForEach(t => _allTags.Remove(t));
+                    AppendData();
+                    //rptTags.DataSource = _selectedtags;
+                    //rptTags.DataBind();
+                    FillForm(selected_id);
+                }
+            }
+        }
+
+        private void AppendData()
+        {
+            ddlTags.DataSource = _allTags;
+            ddlTags.DataValueField = "Id";
+            ddlTags.DataTextField = "Name";
+            ddlTags.DataBind();
+        }
+
+        private void FillForm(int id)
+        {
+            Apartment a = DbAccess.GetApartmentById(id);
+
+            txtName.Text = a.Name;
+        }
 
         protected void btnAddTag_Click(object sender, EventArgs e)
         {
