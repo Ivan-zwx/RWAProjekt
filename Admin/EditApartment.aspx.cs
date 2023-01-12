6061f06b1d6a768b66042594c3a06bd3055bf553
Ivan <someonenetko@hotmail.com>
2023-01-12 20:37:54 +0100

    Fied edit apartment form code behind.

--- Admin/EditApartment.aspx.cs
+++ Admin/EditApartment.aspx.cs
@@ -14,8 +14,8 @@ namespace Admin
         private static IList<City> _allCities;
         private static IList<Status> _allStatuses;
         private static IList<Owner> _allOwners;
-        private static IList<Tag> _allTags;
-        private static ISet<Tag> _selectedtags;
+        //private static IList<Tag> _allTags;
+        //private static ISet<Tag> _selectedtags;
 
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -25,7 +25,7 @@ namespace Admin
             }
             if (!IsPostBack)
             {
-                _allTags = DbAccess.LoadTags();
+                //_allTags = DbAccess.LoadTags();
                 _allCities = DbAccess.GetCities();
                 _allStatuses = DbAccess.GetStatus();
                 _allOwners = DbAccess.GetOwners();
@@ -33,10 +33,10 @@ namespace Admin
                 if (Session["apartment_id"] != null)
                 {
                     int selected_id = (int)Session["apartment_id"];
-                    _selectedtags = new HashSet<Tag>(DbAccess.LoadTagsForApartment(selected_id));
-                    _selectedtags.ToList().ForEach(t => _allTags.Remove(t));
+                    //_selectedtags = new HashSet<Tag>(DbAccess.LoadTagsForApartment(selected_id));
+                    //_selectedtags.ToList().ForEach(t => _allTags.Remove(t));
                     AppendData();
-                    rptTags.DataSource = _selectedtags;
+                    //rptTags.DataSource = _selectedtags;
                     rptTags.DataBind();
                     FillForm(selected_id);
                 }
@@ -62,10 +62,10 @@ namespace Admin
 
         private void AppendData()
         {
-            ddlTags.DataSource = _allTags;
-            ddlTags.DataValueField = "Id";
-            ddlTags.DataTextField = "Name";
-            ddlTags.DataBind();
+            //ddlTags.DataSource = _allTags;
+            //ddlTags.DataValueField = "Id";
+            //ddlTags.DataTextField = "Name";
+            //ddlTags.DataBind();
 
             ddlCity.DataSource = _allCities;
             ddlCity.DataValueField = "Id";
@@ -85,6 +85,7 @@ namespace Admin
 
         protected void btnAddTag_Click(object sender, EventArgs e)
         {
+            /*
             Tag t = _allTags.FirstOrDefault(x => x.Name == ddlTags.SelectedItem.Text);
             if (_selectedtags.Add(t))
             {
@@ -98,15 +99,16 @@ namespace Admin
 
             rptTags.DataSource = _selectedtags;
             rptTags.DataBind();
+            */
         }
 
         protected void btnOdustani_Click(object sender, EventArgs e)
         {
-            _selectedtags.Clear();
+            //_selectedtags.Clear();
             _allCities.Clear();
             _allOwners.Clear();
             _allStatuses.Clear();
-            _selectedtags.Clear();
+            //_selectedtags.Clear();
             Response.Redirect("Apartments.aspx");
         }
 
@@ -126,15 +128,16 @@ namespace Admin
                 Price = decimal.Parse(txtPrice.Text),
                 TotalRooms = int.Parse(txtRoomsCount.Text)
             });
-            _selectedtags.ToList().ForEach(t => DbAccess.AddTaggedApartment(txtName.Text, t.Name));
-            _allTags.ToList().ForEach(t => DbAccess.DeleteTaggedApartment(txtName.Text, t.Name));
-            _selectedtags.Clear();
+            //_selectedtags.ToList().ForEach(t => DbAccess.AddTaggedApartment(txtName.Text, t.Name));
+            //_allTags.ToList().ForEach(t => DbAccess.DeleteTaggedApartment(txtName.Text, t.Name));
+            //_selectedtags.Clear();
             Session["apartment_id"] = null;
             Response.Redirect("Apartments.aspx");
         }
 
         protected void btnRemove_Click(object sender, EventArgs e)
         {
+            /*
             LinkButton button = sender as LinkButton;
             string name = button.CommandArgument;
             Tag t = _selectedtags.FirstOrDefault(x => x.Name == name);
@@ -149,6 +152,7 @@ namespace Admin
 
             rptTags.DataSource = _selectedtags;
             rptTags.DataBind();
+            */
         }
 
         protected void txtName_TextChanged(object sender, EventArgs e)
