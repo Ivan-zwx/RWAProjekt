3d5a2496004c5e8763e92dff585391f36d0afdd4
Ivan <someonenetko@hotmail.com>
2023-01-15 21:05:51 +0100

    Cleaned up edit apartments form - old unused tag related code from both aspx and codebehind.

--- Admin/EditApartment.aspx.cs
+++ Admin/EditApartment.aspx.cs
@@ -14,8 +14,6 @@ namespace Admin
         private static IList<City> _allCities;
         private static IList<Status> _allStatuses;
         private static IList<Owner> _allOwners;
-        //private static IList<Tag> _allTags;
-        //private static ISet<Tag> _selectedtags;
 
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -25,7 +23,6 @@ namespace Admin
             }
             if (!IsPostBack)
             {
-                //_allTags = DbAccess.LoadTags();
                 _allCities = DbAccess.GetCities();
                 _allStatuses = DbAccess.GetStatus();
                 _allOwners = DbAccess.GetOwners();
@@ -33,11 +30,7 @@ namespace Admin
                 if (Session["apartment_id"] != null)
                 {
                     int selected_id = (int)Session["apartment_id"];
-                    //_selectedtags = new HashSet<Tag>(DbAccess.LoadTagsForApartment(selected_id));
-                    //_selectedtags.ToList().ForEach(t => _allTags.Remove(t));
                     AppendData();
-                    //rptTags.DataSource = _selectedtags;
-                    rptTags.DataBind();
                     FillForm(selected_id);
                 }
             }
@@ -62,11 +55,6 @@ namespace Admin
 
         private void AppendData()
         {
-            //ddlTags.DataSource = _allTags;
-            //ddlTags.DataValueField = "Id";
-            //ddlTags.DataTextField = "Name";
-            //ddlTags.DataBind();
-
             ddlCity.DataSource = _allCities;
             ddlCity.DataValueField = "Id";
             ddlCity.DataTextField = "Name";
@@ -85,30 +73,13 @@ namespace Admin
 
         protected void btnAddTag_Click(object sender, EventArgs e)
         {
-            /*
-            Tag t = _allTags.FirstOrDefault(x => x.Name == ddlTags.SelectedItem.Text);
-            if (_selectedtags.Add(t))
-            {
-                _allTags.Remove(t);
-            }
-
-            ddlTags.DataSource = _allTags;
-            ddlTags.DataValueField = "Id";
-            ddlTags.DataTextField = "Name";
-            ddlTags.DataBind();
-
-            rptTags.DataSource = _selectedtags;
-            rptTags.DataBind();
-            */
         }
 
         protected void btnOdustani_Click(object sender, EventArgs e)
         {
-            //_selectedtags.Clear();
             _allCities.Clear();
             _allOwners.Clear();
             _allStatuses.Clear();
-            //_selectedtags.Clear();
             Response.Redirect("Apartments.aspx");
         }
 
@@ -128,36 +99,16 @@ namespace Admin
                 Price = decimal.Parse(txtPrice.Text),
                 TotalRooms = int.Parse(txtRoomsCount.Text)
             });
-            //_selectedtags.ToList().ForEach(t => DbAccess.AddTaggedApartment(txtName.Text, t.Name));
-            //_allTags.ToList().ForEach(t => DbAccess.DeleteTaggedApartment(txtName.Text, t.Name));
-            //_selectedtags.Clear();
             Session["apartment_id"] = null;
             Response.Redirect("Apartments.aspx");
         }
 
         protected void btnRemove_Click(object sender, EventArgs e)
         {
-            /*
-            LinkButton button = sender as LinkButton;
-            string name = button.CommandArgument;
-            Tag t = _selectedtags.FirstOrDefault(x => x.Name == name);
-            _allTags.Add(t);
-            _selectedtags.Remove(t);
-
-            ddlTags.DataSource = _allTags;
-            ddlTags.DataValueField = "Id";
-            ddlTags.DataTextField = "Name";
-            ddlTags.DataBind();
-
-
-            rptTags.DataSource = _selectedtags;
-            rptTags.DataBind();
-            */
         }
 
         protected void txtName_TextChanged(object sender, EventArgs e)
         {
-
         }
     }
 }
--- Admin/EditApartment.aspx.designer.cs
+++ Admin/EditApartment.aspx.designer.cs
@@ -213,42 +213,6 @@ namespace Admin
         protected global::System.Web.UI.WebControls.DropDownList ddlOwner;
 
         /// <summary>
-        /// Label3 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label3;
-
-        /// <summary>
-        /// ddlTags control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.DropDownList ddlTags;
-
-        /// <summary>
-        /// btnAddTag control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnAddTag;
-
-        /// <summary>
-        /// rptTags control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Repeater rptTags;
-
-        /// <summary>
         /// btnSpremi control.
         /// </summary>
         /// <remarks>
