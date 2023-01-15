9d0e614b41e9a7ad6ad2662a22e16dbb113ee4ca
Ivan <someonenetko@hotmail.com>
2023-01-15 02:15:28 +0100

    THIS FINALLY WORKS - REVIEW SUBMIT IMPLEMENTED AND TESTED. THE ISSUE WAS CONTROLLER INSTANCING.

--- User/Controllers/ReviewController.cs
+++ User/Controllers/ReviewController.cs
@@ -2,6 +2,7 @@
 using DAL.DatabaseAccess;
 using DAL.Models;
 using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
@@ -12,7 +13,8 @@ namespace User.Controllers
 {
     public class ReviewController : Controller
     {
-        List<Apartment> reservedApartments;
+        // NIJE RADILO ZBOG PONOVNOG INSTANCIRANJA KONTROLERA IZMEDU DVA ACTIONRESULTA - STATIC VARIABLA JE QUICK FIX RJESENJE - TREBA NACI BOLJE
+        //static List<Apartment> reservedApartments;
 
         // GET: Review
         public ActionResult Index()
@@ -21,9 +23,7 @@ namespace User.Controllers
             {
                 // load apartments that the user has reserved in the past from the database
                 // note: submit button on form should be disabled if user has no reservations or none are selected
-                var user = (DAL.Models.User)Session["user"];
-                ViewBag.User = user;
-                reservedApartments = DbAccess.QueryReservedApartmentsForUser(user.Id).ToList();
+                var reservedApartments = queryReserverApartments();
                 return View(reservedApartments);
             }
             else
@@ -33,10 +33,11 @@ namespace User.Controllers
         }
 
         [HttpPost]
-        public ActionResult SubmitReview(int userId, string apartmentName, string details, int stars)
+        public ActionResult SubmitReview(int userId, string apartmentName, int stars, string details)
         {
             try
             {
+                var reservedApartments = queryReserverApartments();
                 if (reservedApartments != null && reservedApartments.Count() != 0)
                 {
                     int apartmentId = reservedApartments.Where(x => x.Name == apartmentName).FirstOrDefault().Id;
@@ -47,7 +48,14 @@ namespace User.Controllers
             {
                 return Json("Slanje recenzije nije uspjelo");
             }
-            return Json("Recenzija je poslana");
+            return Json("Recenzija je uspjesno poslana");
+        }
+
+        private List<Apartment> queryReserverApartments()
+        {
+            var user = (DAL.Models.User)Session["user"];
+            ViewBag.User = user;
+            return DbAccess.QueryReservedApartmentsForUser(user.Id).ToList();
         }
     }
 }
